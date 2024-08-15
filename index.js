2ef5b72b8129a9401215bfdaf81c441b7cab4a99
utav8292 <utsav8292@gmail.com>
2024-08-16 01:41:40 +0530

    ff

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["https://portal-f.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
